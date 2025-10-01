81db1db9e014119367e0968254dc9d999d63db07
Sashank Kharal <skharal3@myseneca.ca>
2025-10-01 16:06:02 -0400

    Completed Lab

---
+++ lab4e.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Author ID: 137379236
+
+def is_digits(sobj):
+    if sobj == '':
+        return False
+    for ch in sobj:
+        if ch not in '0123456789':
+            return False
+    return True
+
+if __name__ == '__main__':
+    test_list = ['x3058','3058','8503x','8503']
+    for item in test_list:
+        if is_digits(item):
+            print(item,'is an integer.')
+        else:
+            print(item,'is not an integer.')
